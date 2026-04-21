e54f29c2ff71ef9d5390a1544b24c9ed673ff6b5
AdamMyrenNDSU <adam.myren@ndsu.edu>
2026-04-20 20:06:59 -0500

    Made category page look nice

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { DashboardComponent } from './dashboard.component/dashboard.component';
 import { AuthService } from './auth.service';
 import { Category } from './category/category.component';
+import { CategoryService } from './category.service';
 
 @Component({
   selector: 'app-root',
@@ -22,4 +23,17 @@ export class App {
   protected readonly title = signal('assignment5');
 
   public auth = inject(AuthService);
+
+  catService = inject(CategoryService);
+
+  constructor() {
+    // This effect runs every time auth.currentUser changes
+    effect(() => {
+      const user = this.auth.currentUser();
+      if (user) {
+        console.log('User detected on refresh, starting category listener...');
+        this.catService.listenToCategories(user.uid);
+      }
+    });
+  }
 }
